9d2473633de12e9e123ad9c9175e6953920fb6f0
John Ryan <john.p.ryan4@gmail.com>
2018-03-14 22:40:27 -0700

    try new travis build

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,18 @@
 echo "building jekyll blog"
 jekyll build
 
-echo "deploying"
-cd _site
-git init
-git config user.name "Travis CI"
-git config user.email "john.p.ryan4@gmail.com"
 
-git remote add pages https://github.com/johnpryan/johnpryan.github.com.git
-
-DATE=`date +'%m/%d/%Y'`
-TIME=`date +'%r'`
-
-git add .
-git commit -m "$DATE $TIME"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master 2>&1
+#echo "deploying"
+#cd _site
+#git init
+#git config user.name "Travis CI"
+#git config user.email "john.p.ryan4@gmail.com"
+#
+#git remote add pages https://github.com/johnpryan/johnpryan.github.com.git
+#
+#DATE=`date +'%m/%d/%Y'`
+#TIME=`date +'%r'`
+#
+#git add .
+#git commit -m "$DATE $TIME"
+#git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master 2>&1
